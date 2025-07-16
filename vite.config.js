d6dae31a25fd49bf182b2f2cee5218b471ca8353
pujanjoci <pujanjoci2@gmail.com>
2025-07-16 16:36:12 +0545

    modified basename

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/everestclaimsandadvisor/', // Adjust this if your project is hosted in a subdirectory
+  base: '/everestclaimsandadvisory/', // Adjust this if your project is hosted in a subdirectory
   plugins: [
     react(),
     tailwindcss(),
